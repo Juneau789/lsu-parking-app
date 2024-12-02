1919abddacd6b3897b030c7a9176f8079c3b8065
Juneau789 <hjunea9@gmail.com>
2024-12-02 12:18:32 -0600

    some changes

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,24 @@ const ParkingLot = require('../models/ParkingLot');
 
 // Availability Page
 router.get('/availability', async (req, res) => {
-  const parkingLots = await ParkingLot.find();
-  res.render('pages/availability', { parkingLots });
+  try {
+    const parkingLots = await ParkingLot.find();
+    res.render('pages/availability', { parkingLots });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 // Admin Page
-router.get('/admin', (req, res) => {
-  res.render('pages/admin');
+router.get('/admin', async (req, res) => {
+  try {
+    const parkingLots = await ParkingLot.find(); // Fetch parking lots
+    res.render('pages/admin', { parkingLots });  // Pass parkingLots to the view
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 module.exports = router;
